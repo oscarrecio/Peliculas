acc1a33bc8406087efb1c4a79bd2b1909756d8e3
Oscar Recio <oscar.recio16@gmail.com>
2021-03-03 08:25:46 -0700

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front-end';
+export class AppComponent implements OnInit{
+  peliculasEnCartelera: any;
+  peliculasProximosEstrenos: any;
+
+  ngOnInit(){
+    
+    setTimeout(()=>{
+      this.peliculasEnCartelera = [{
+        titulo: 'Spider-Man',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99
+      },
+      {
+        titulo: 'Moana',
+        fechaLanzamiento: new Date(),
+        precio: 3710.99
+      }];
+
+      this.peliculasProximosEstrenos = [{
+        titulo: 'Avengers EndGame',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99
+      },
+      {
+        titulo: 'El Origen',
+        fechaLanzamiento: new Date(),
+        precio: 3710.99
+      },
+      {
+        titulo: 'Blanca Nieves',
+        fechaLanzamiento: new Date(),
+        precio: 3710.99
+      }];
+    }, 800);
+  }
+
+  
+
 }
---
+++ src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-peliculas',
+  templateUrl: './listado-peliculas.component.html',
+  styleUrls: ['./listado-peliculas.component.css']
+})
+export class ListadoPeliculasComponent implements OnInit {
+
+  constructor() { }
+  @Input()
+  peliculas: any = [];
+
+  ngOnInit(): void {
+  }
+
+  remover(indexMovie: number){
+    this.peliculas.splice(indexMovie,1);
+  }
+
+}
